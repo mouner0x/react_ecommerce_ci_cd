5383d8c9eb95b88c658c980141db1a52a9bf0cb6
Mouner Saleh <95081593+mouner0x@users.noreply.github.com>
2025-07-16 20:09:13 +0300

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -12,7 +12,7 @@ const Checkout = () => {
           <div className="col-md-12 py-5 bg-light text-center">
             <h4 className="p-3 display-5">No item in Cart</h4>
             <Link to="/" className="btn btn-outline-dark mx-4">
-              <i className="fa fa-arrow-left"></i> Continue Shopping
+              <i className="fa fa-arrow-left"></i> Continue 
             </Link>
           </div>
         </div>
